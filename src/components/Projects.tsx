a336428ed5c36ac936d4c6ca8cb7c510413a2d89
William McQuaid <whmcquaid@gmail.com>
2021-06-24 19:08:04 -0400

    added projects adjusted CSS

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,8 @@ const restOfProps = {
   position: 'absolute',
   height: '400px',
   width: '400px',
+  border: '8px solid silver',
+  paddingBottom: '25px',
 };
 const props1 = {
   src: 'https://whmcquaid.github.io/React-RMDB/',
@@ -32,24 +34,36 @@ const Projects = () => {
   return (
     <>
       <div className='projects'>
-        <h3>React Movie App</h3>
-        <h4>JavaScript, React, HTML, CSS</h4>
-        <iframe title='React RMDB' {...props1}></iframe>
-        <h3>Rock Paper Scissors</h3>
-        <h4>JavaScript, React, HTML, CSS</h4>
-        <iframe title='Rock Paper Scissors' {...props2}></iframe>
-        <h3>Tic Tac Toe</h3>
-        <h4>JavaScript, React, HTML, CSS</h4>
-        <iframe title='Tic Tac Toe' {...props3}></iframe>
-        <h3>Etch A Sketch</h3>
-        <h4>JavaScript, HTML, CSS</h4>
-        <iframe title='Etch a Sketch' {...props4}></iframe>
-        <h3>Library App</h3>
-        <h4>JavaScript, HTML, CSS</h4>
-        <iframe title='Library App' {...props5}></iframe>
-        <h3>Todo List App</h3>
-        <h4>TypeScript, React, HTML, CSS</h4>
-        <iframe title='Todo List App' {...props6}></iframe>
+        <a href='http://whmcquaid.github.io/React-RMDB/'>
+          <h3>React Movie App</h3>
+          <h4>JavaScript, React, HTML, CSS</h4>
+          <iframe title='React RMDB' {...props1}></iframe>
+        </a>
+        <a href='http://whmcquaid.github.io/rock_paper_scissors'>
+          <h3>Rock Paper Scissors</h3>
+          <h4>JavaScript, React, HTML, CSS</h4>
+          <iframe title='Rock Paper Scissors' {...props2}></iframe>
+        </a>
+        <a href='http://whmcquaid.github.io/tic-tac-toe'>
+          <h3>Tic Tac Toe</h3>
+          <h4>JavaScript, React, HTML, CSS</h4>
+          <iframe title='Tic Tac Toe' {...props3}></iframe>
+        </a>
+        <a href='http://whmcquaid.github.io/etch_a_sketch'>
+          <h3>Etch A Sketch</h3>
+          <h4>JavaScript, HTML, CSS</h4>
+          <iframe title='Etch a Sketch' {...props4}></iframe>
+        </a>
+        <a href='http://whmcquaid.github.io/library_app'>
+          <h3>Library App</h3>
+          <h4>JavaScript, HTML, CSS</h4>
+          <iframe title='Library App' {...props5}></iframe>
+        </a>
+        <a href='http://whmcquaid.github.io/todo_list'>
+          <h3>Todo List App</h3>
+          <h4>TypeScript, React, HTML, CSS</h4>
+          <iframe title='Todo List App' {...props6}></iframe>
+        </a>
       </div>
     </>
   );
