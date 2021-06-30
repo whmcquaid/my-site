77154d4a4d102a6e46465f9f178136c468ad40ad
William McQuaid <whmcquaid@gmail.com>
2021-06-29 20:18:06 -0400

    updated about paragraph text more

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,7 +15,10 @@ const About = () => {
             learn and I am passionate about coding. Right now, I work a
             full-time job cleaning, and repairing pools. Given the opportunity,
             I would love to work with a team of developers in a collaborative
-            manner.
+            manner. When I was in middle school I created my own guild website
+            for my favorite game, diablo, by teaching myself html. I also
+            learned some visual basic and turbo pascal. From then on I knew I
+            wanted to be in software development.
           </p>
         </div>
       </div>
