51b93034475da735228d72db63f31af5c05bc16f
William McQuaid <whmcquaid@gmail.com>
2021-08-10 22:36:54 -0400

    added padding-bottom 75px to project component to show last project description

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,19 +4,19 @@ import Descriptions, { Desc } from './Descriptions';
 
 const ProjectCard = () => {
   return (
-    <>
+    <div className='projects'>
       <h1 className='projectsh1'>Projects</h1>
       {Descriptions.map((item: Desc, index: number) => (
-        <div className='projects'>
+        <>
           <a href={FrameProps[index].src}>
             <h2>{FrameProps[index].title}</h2>
           </a>
           <ProjectDescription {...item} />
           {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
           {/*    {window.innerWidth > 700 && <iframe {...FrameProps[index]}></iframe>} */}
-        </div>
+        </>
       ))}
-    </>
+    </div>
   );
 };
 export default ProjectCard;
