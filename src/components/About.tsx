7a4e22fa89eb8af90cde032cad11d2ca3f7b519d
William McQuaid <whmcquaid@gmail.com>
2021-08-13 22:55:08 -0400

    fixed typo on about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,20 +12,20 @@ const About = () => {
         </div>
         <div className='right-col'>
           <p id='about'>
-            Hello, my name is William and I am a full stack Developer. I love to
+            Hello, my name is William and I am a full stack developer. I love to
             learn and I am passionate about coding. Right now, I work a
-            full-time job cleaning, and repairing pools. Given the opportunity,
-            I would love to work with a team of developers in a collaborative
-            manner. I have always been a very curious person. Part of the reason
-            that I really enjoy software delelopment is that I never stop
-            learning. Many times I have come across unfamiliar problems and I
-            find them intriguing. I may not know how to solve a particular
-            problem at this moment, but I can confidently say that I will figure
-            it out. I really enjoy going down the rabbit hole that is often part
-            of the problem solving process, and learning something brand new
-            that I get to try out. My persistance is not easily swayed so I can
-            be found scouring MDN or stack overflow with a whole plethora of
-            browser tabs open until said problem is dealt with. The
+            full-time job cleaning, and repairing pools. But not for long! Given
+            the opportunity, I would love to work with a team of developers in a
+            collaborative manner. I have always been a very curious person. Part
+            of the reason that I really enjoy software delelopment is that I
+            never stop learning. Many times I have come across unfamiliar
+            problems and I find them intriguing. I may not know how to solve a
+            particular problem at this moment, but I can confidently say that I
+            will figure it out. I really enjoy going down the rabbit hole that
+            is often part of the problem solving process, and learning something
+            brand new that I get to try out. My persistance is not easily swayed
+            so I can be found scouring MDN or stack overflow with a whole
+            plethora of browser tabs open until said problem is dealt with. The
             gratification I get from solving problems with code is one of my
             driving forces. If you would like to get to know more about me
             please <Link to='/Contact'>reach out.</Link>
