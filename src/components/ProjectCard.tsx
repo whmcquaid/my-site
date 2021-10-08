dfa73d2a9241169c55e7d3ef2f2dea3902651b8e
William McQuaid <whmcquaid@gmail.com>
2021-10-08 18:14:18 -0400

    Improved layout - cards mobile responsive

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,18 +4,20 @@ import Descriptions, { Desc } from './Descriptions';
 
 const ProjectCard = () => {
   return (
-    <div className='projects'>
-      <h1 className='projectsh1'>Projects</h1>
-      {Descriptions.map((item: Desc, index: number) => (
-        <>
-          <a href={FrameProps[index].src}>
-            <h2>{FrameProps[index].title}</h2>
-          </a>
-          <ProjectDescription {...item} />
-          {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
-          {/*    {window.innerWidth > 700 && <iframe {...FrameProps[index]}></iframe>} */}
-        </>
-      ))}
+    <div>
+      <h1 className="projectsh1">Projects</h1>
+      <div className="projects">
+        {Descriptions.map((item: Desc, index: number) => (
+          <div className="card">
+            <h2 className="cardTitle">
+              <a href={FrameProps[index].src}>{FrameProps[index].title}</a>
+            </h2>
+            <ProjectDescription {...item} />
+            {/* eslint-disable-next-line jsx-a11y/iframe-has-title */}
+            {/*    {window.innerWidth > 700 && <iframe {...FrameProps[index]}></iframe>} */}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/ProjectDescription.tsx
+++ src/components/ProjectDescription.tsx
@@ -2,12 +2,10 @@ import { Desc } from './Descriptions';
 
 export default function ProjectDescription(props: Desc) {
   return (
-    <div className='description'>
+    <div className="description">
+      <p>{props.description}</p>
       <p>
         <strong>Technologies utilized:</strong> {props.tech}
-      </p>
-      <p>
-        <strong>Project Description:</strong> {props.description}
       </p>
     </div>
   );
